26fa9e9d1537e0670491de9d32330dcc78d07a8c
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2025-05-11 14:20:57 +0300

    Change primary color to purple

--- practice-app/mobile/app/_layout.tsx
+++ practice-app/mobile/app/_layout.tsx
@@ -7,16 +7,34 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+const myLightTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#636AE8', // purple
+  },
+};
+
+const myDarkTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: '#636AE8', // purple
+  },
+};
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    // The below should be 'dark', but it can be changed to 'light' for testing
+    <ThemeProvider value={colorScheme === 'dark' ? myDarkTheme : myLightTheme}>
       <Stack screenOptions={{ headerShown: false }}>
         {/* Plain Stack—no bottom tabs */}
         <Stack.Screen name="index" />
         <Stack.Screen name="signin" />
         <Stack.Screen name="signup" />
+        <Stack.Screen name="figmaComponentPage" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
