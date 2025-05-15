b6cad240f7eda763f2363fa34ca8d622a3108be2
sonwezali <al1.sonmez.mi@gmail.com>
2025-05-15 19:22:54 +0300

    update 'localhost' to the server ip

--- app/frontend/src/services/authService.js
+++ app/frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base API URL
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'http://165.227.152.202:8000/api';
 
 /**
  * Handle login response by storing token and user info
