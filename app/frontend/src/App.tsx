19a81f0758f55b954f749d41bbc1fbb89b0e7079
musakaangny <musakaangny@gmail.com>
2025-09-28 13:45:37 +0300

    feat: add Forgot Password and Reset Password pages with routing

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './App.css'
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
         {/* Redirect root to /login */}
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
