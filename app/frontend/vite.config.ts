0caa820e6fc512bdaaba9b5b3c016fe47b77710e
erayyuklu <erayyuklu@gmail.com>
2025-10-28 01:18:44 +0300

    fix(deploy): update the vite's host settings.

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: ['localhost', '172.18.0.4', 'neighborhood.chickenkiller.com', 'frontend']
   }
 })
